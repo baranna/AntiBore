84facc003d2740561aca7ec28a4c794bac1b8d73
Baross Anna <baross.anna@gmail.com>
2021-05-08 22:32:38 +0200

    show occupation list

--- app/src/main/java/hu/mobillab/antibore/ui/main/MainActivity.kt
+++ app/src/main/java/hu/mobillab/antibore/ui/main/MainActivity.kt
@@ -1,14 +1,37 @@
 package hu.mobillab.antibore.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import dagger.hilt.android.AndroidEntryPoint
 import hu.mobillab.antibore.R
+import hu.mobillab.antibore.model.Category
 import hu.mobillab.antibore.model.Occupation
-import hu.mobillab.antibore.ui.occupation.OccupationPresenter
+import hu.mobillab.antibore.ui.components.Loading
+import hu.mobillab.antibore.ui.components.ThemeProvider
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), MainScreen {
@@ -16,10 +39,22 @@ class MainActivity : AppCompatActivity(), MainScreen {
     @Inject
     lateinit var mainPresenter: MainPresenter
 
+    private val occupations = mutableStateListOf<Occupation>()
+
+    private val isRefreshing = mutableStateOf(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         mainPresenter.getOccupations()
+        setContent {
+            ThemeProvider {
+                if (occupations.size == 0) {
+                    Loading()
+                } else {
+                    OccupationList(occupations)
+                }
+            }
+        }
     }
 
     override fun onStart() {
@@ -33,8 +68,69 @@ class MainActivity : AppCompatActivity(), MainScreen {
     }
 
     override fun showOccupations(occupationsList: List<Occupation?>) {
-        occupationsList.forEach {
-            Log.d("Result", it.toString())
+        occupations.clear()
+        occupations.addAll(occupationsList.filterNotNull())
+        isRefreshing.value = false
+    }
+
+    @Composable
+    fun OccupationList(
+        occupations: SnapshotStateList<Occupation>
+    ) {
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(isRefreshing.value),
+            onRefresh = { isRefreshing.value = true; mainPresenter.getOccupations() }
+        ) {
+            LazyColumn() {
+                items(occupations) {
+                    OccupationListItem(occupation = it)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun OccupationListItem(occupation: Occupation) {
+        Card(
+            elevation = 3.dp,
+            shape = RoundedCornerShape(3.dp),
+            modifier = Modifier
+                .clickable { mainPresenter.onOccupationClicked(occupation.key) }
+                .fillMaxWidth()
+                .padding(top = 8.dp, start = 4.dp, end = 4.dp, bottom = 5.dp)
+                .defaultMinSize(minHeight = 65.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = getIconForCategory(occupation.type),
+                    contentDescription = stringResource(R.string.categoryIcon),
+                    tint = MaterialTheme.colors.secondary,
+                    modifier = Modifier
+                        .size(45.dp)
+                        .padding(start = 2.dp)
+                )
+                Text(
+                    text = occupation.activity,
+                    style = MaterialTheme.typography.subtitle1,
+                    modifier = Modifier.padding(start = 15.dp)
+                )
+            }
+        }
+    }
+
+    private fun getIconForCategory(type: Category): ImageVector {
+        return when (type) {
+            Category.EDUCATION -> Icons.Outlined.School
+            Category.RECREATIONAL -> Icons.Outlined.SelfImprovement
+            Category.SOCIAL -> Icons.Outlined.Groups
+            Category.DIY -> Icons.Outlined.Handyman
+            Category.CHARITY -> Icons.Outlined.VolunteerActivism
+            Category.COOKING -> Icons.Outlined.RamenDining
+            Category.RELAXATION -> Icons.Outlined.Spa
+            Category.MUSIC -> Icons.Outlined.MusicNote
+            Category.BUSYWORK -> Icons.Outlined.HistoryEdu
         }
     }
 }
--- app/src/main/java/hu/mobillab/antibore/ui/main/MainPresenter.kt
+++ app/src/main/java/hu/mobillab/antibore/ui/main/MainPresenter.kt
@@ -1,12 +1,28 @@
 package hu.mobillab.antibore.ui.main
 
+import android.content.Context
+import android.content.Intent
 import hu.mobillab.antibore.interactors.OccupationInteractor
 import hu.mobillab.antibore.ui.Presenter
+import hu.mobillab.antibore.ui.occupation.OccupationActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainPresenter @Inject constructor(var occupationInteractor: OccupationInteractor) : Presenter<MainScreen?> () {
+class MainPresenter @Inject constructor(var occupationInteractor: OccupationInteractor) :
+    Presenter<MainScreen?>() {
 
-    fun getOccupations()  {
-        return occupationInteractor.getOccupations()
+    fun getOccupations() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val occupations = occupationInteractor.getOccupations()
+            screen?.showOccupations(occupations)
+        }
+    }
+
+    fun onOccupationClicked(id: String) {
+        val intent = Intent(screen as Context, OccupationActivity::class.java);
+        intent.putExtra("id", id)
+        (screen as Context).startActivity(intent)
     }
 }
