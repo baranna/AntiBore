38257eb803225b2d1d3eb749655ddb4c59b4ac11
Baross Anna <baross.anna@gmail.com>
2021-05-08 22:33:06 +0200

    show occupation details

--- app/src/main/java/hu/mobillab/antibore/interactors/OccupationInteractor.kt
+++ app/src/main/java/hu/mobillab/antibore/interactors/OccupationInteractor.kt
@@ -4,33 +4,29 @@ import hu.mobillab.antibore.model.Category
 import hu.mobillab.antibore.model.Occupation
 import hu.mobillab.antibore.network.OccupationApi
 import hu.mobillab.antibore.network.dto.OccupationDto
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import hu.mobillab.antibore.repository.OccupationRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class OccupationInteractor @Inject constructor(var occupationApi: OccupationApi, var occupationRepository: OccupationRepository) {
+class OccupationInteractor @Inject constructor(
+    var occupationApi: OccupationApi,
+    var occupationRepository: OccupationRepository
+) {
 
-    fun getOccupations() {
-        // TODO: get stored occupations and get new ones
-        CoroutineScope(Dispatchers.IO).launch {
-            occupationRepository.getAllOccupations()
-            occupationApi.getActivity()
+    suspend fun getOccupations(): List<Occupation> {
+        occupationRepository.getAllOccupations()
+        val list = mutableListOf<Occupation>()
+        for (i in 1..10) {
+            list.add(mapOccupation(occupationApi.getActivity()))
         }
+        return list;
     }
 
-    fun getOccupation(key: String = "") {
-        CoroutineScope(Dispatchers.IO).launch {
-            occupationRepository.getAllOccupations()
-            occupationApi.getActivity(key)
-        }
+    suspend fun getOccupation(key: String? = ""): Occupation {
+        occupationRepository.getAllOccupations()
+        return mapOccupation(occupationApi.getActivity(key))
     }
 
-    fun mapOccupation(dto: OccupationDto) = Occupation(
+    private fun mapOccupation(dto: OccupationDto) = Occupation(
         dto.key ?: "",
         dto.activity ?: "",
         dto.accessibility?.toDouble() ?: 0.0,
--- app/src/main/java/hu/mobillab/antibore/ui/occupation/OccupationPresenter.kt
+++ app/src/main/java/hu/mobillab/antibore/ui/occupation/OccupationPresenter.kt
@@ -1,12 +1,27 @@
 package hu.mobillab.antibore.ui.occupation
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import hu.mobillab.antibore.interactors.OccupationInteractor
 import hu.mobillab.antibore.ui.Presenter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
- class OccupationPresenter @Inject constructor(var occupationInteractor: OccupationInteractor) : Presenter<OccupationScreen?>() {
+class OccupationPresenter @Inject constructor(var occupationInteractor: OccupationInteractor) :
+    Presenter<OccupationScreen?>() {
 
-     fun getOccupation(key: String) {
-         return occupationInteractor.getOccupation(key)
-     }
- }
+    fun getOccupation(key: String? = "") {
+        CoroutineScope(Dispatchers.IO).launch {
+            val occupation = occupationInteractor.getOccupation(key)
+            screen?.showOccupationDetails(occupation)
+        }
+    }
+
+    fun openWebsite(url: String) {
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        (screen as Context).startActivity(webIntent)
+    }
+}
